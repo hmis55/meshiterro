1ead793d36d638958634d49d4df7c7431c2f9cbf
hmis55 <158746092+hmis55@users.noreply.github.com>
2024-02-19 14:58:12 +0000

    ページネーション実装

--- app/models/post_image.rb
+++ app/models/post_image.rb
@@ -1,16 +1,13 @@
 class PostImage < ApplicationRecord
 
- has_one_attached :image
  belongs_to :user
- has_many :post_comments, dependent: :destroy 
+ has_one_attached :image
+ has_many :post_comments, dependent: :destroy
+ has_many :favorites, dependent: :destroy
+ 
+  validates :shop_name, presence: true
+  validates :image, presence: true
 
- def get_image
-   if image.attached?
-     image
-   else
-     'no_image.jpg'
-   end
- end
 
  def get_image
    unless image.attached?
@@ -19,5 +16,8 @@ class PostImage < ApplicationRecord
    end
    image
  end
-
+ 
+ def favorited_by?(user)
+  favorites.exists?(user_id: user.id)
+ end
 end
